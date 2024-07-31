d60b8004bb856687919150d8a423616db5f709cf
Grey Boolean <greyboolean@gmail.com>
2024-07-31 19:15:23 +0530

    fix: Resolve session storage not available on server error

--- src/hooks/useSessionStorage.ts
+++ src/hooks/useSessionStorage.ts
@@ -5,8 +5,11 @@ function useSessionStorage<T>(
 	initialState: T
 ): [T, React.Dispatch<React.SetStateAction<T>>] {
 	const [value, setValue] = useState(() => {
-		const storedValue = sessionStorage.getItem(key);
-		return storedValue ? JSON.parse(storedValue) : initialState;
+		if (typeof window !== 'undefined') {
+      const storedValue = sessionStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : initialState;
+    }
+    return initialState;
 	});
 
 	useEffect(() => {
